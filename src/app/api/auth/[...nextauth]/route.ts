b24fa3fe856e548cfe1388c552ae2725913045ac
Swings0 <christianemmanuel641@gmail.com>
2025-01-20 16:19:40 -1100

    Stash #1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions"; // Import the auth options from a separate file
-import { NextApiHandler } from "next";
 
-export const handler = NextAuth(authOptions) as NextApiHandler;
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
