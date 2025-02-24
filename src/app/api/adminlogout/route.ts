777df55021eb2a06f95a15f9d5b8dfb47016d2a8
Swings0 <christianemmanuel641@gmail.com>
2025-02-24 18:12:58 +0100

    Omg

--- src/app/api/adminlogout/route.ts
+++ src/app/api/adminlogout/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST() {
   const response = NextResponse.json({ message: "Logged out" });
-  // Remove the admin-token cookie by setting it to an empty string with an expiration in the past.
   response.cookies.set("admin-token", "", {
     path: "/adminpage",
     expires: new Date(0),
